dd74a6a65c9d60690ff22ae484d6ac0d1e06bac2
fangigun <furkanangigun@gmail.com>
2022-11-08 10:29:04 +0300

    hataları düzeltme+model

--- ln/admin.py
+++ ln/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
-
+from ln.models import Price
 # Register your models here.
+
+class PriceAdmin(admin.ModelAdmin):
+    list_display= ['title','price']
+    list_filter= ['title']
+    list_display_links=['title']
+
+
+
+class Meta:
+
+
+    model=Price
+
+
+
+    admin.site.register(Price,PriceAdmin)
+
+
+
+
+
+
+
+
+
+
--- ln/models.py
+++ ln/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Price(models.Model):
+    title = models.CharField(max_length=30) 
+    price = models.CharField(max_length=10) 
+    dahil1 = models.CharField(max_length=50)
+    dahil2 = models.CharField(max_length=50) 
+    dahil3 = models.CharField(max_length=50) 
+    dahil4 = models.CharField(max_length=50) 
+    dahil5 = models.CharField(max_length=50) 
+    dahil6 = models.CharField(max_length=50)  
+    dahil7 = models.CharField(max_length=50) 
+    image=models.ImageField(upload_to=price,null=True,blank=True)
+
+    def __str__(self):
+        return self.title
+    
